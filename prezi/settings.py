478a8b9844e06113fa3a9e63126fb2375e62bcbc
Giancarlo Fusiello <carlofusiello@gmail.com>
2018-07-23 16:56:11 +0200

    create models

--- prezi/settings.py
+++ prezi/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third-party apps
-    'restframework',
+    'rest_framework',
     # project apps
     'restapi'
 ]
--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=120)
+
+class Prezi(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.URLField()
+    title = models.CharField(max_length=120)
+    created_at = models.DateTimeField()
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
