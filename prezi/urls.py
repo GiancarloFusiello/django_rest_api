e5b5980a55164292f5c3a84f587a38736716469f
Giancarlo Fusiello <carlofusiello@gmail.com>
2018-07-25 15:54:18 +0200

    remove admin web view

--- prezi/urls.py
+++ prezi/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, re_path
 from rest_framework import routers
 
 from restapi import views
@@ -10,5 +9,4 @@ router.register(r'prezis', views.PreziAPIView, base_name='prezis')
 urlpatterns = [
     re_path(r'^api/', include(router.urls)),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
 ]
